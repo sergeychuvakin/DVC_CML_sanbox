3c6384ad089a263867f24e7f622174d93575898d
Sergey <33733128+sergeychuvakin@users.noreply.github.com>
2021-08-12 01:26:18 +0300

    train add

---
+++ train.py
@@ -0,0 +1,29 @@
+"""Train model"""
+import pandas as pd
+import swifter
+import numpy as np
+from config import Config
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+import pickle
+
+config = Config()
+
+df = pd.read_parquet(config.TRAIN_VECTORIZED)
+
+X_train, X_test, y_train, y_test = train_test_split(np.stack(df.texts), df.label)
+
+clf = RandomForestClassifier(max_depth=10, random_state=0)
+clf.fit(X_train, y_train)
+
+
+
+with open("clf.pkl", "wb") as f:
+    pickle.dump(clf, f)
+
+
+
+
+
+pd.DataFrame({"texts": X_test.tolist(), "labels": y_test}).to_parquet(config.TRAIN_VALID)
