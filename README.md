99e05dd9c206feae126167689002e7ba68fe2af5
Sergey Chuvakin <sergeychuvakin1@mail.ru>
2021-08-12 15:06:09 +0300

    experiments

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Here you can find basic ML pipeline that can be used to configure you DVC-CML infrastructure
 reqs:
 pandas swifter stopwords nltk numpy sklearn spacy pyarrow fastparquet
+
+1) git ignore manually 
+*.pkl
+*.parquet
+*.csv
+2) git ignore auto 
+/metrics.txt
+3) git dvc.lock
--- get_data.py
+++ get_data.py
@@ -6,5 +6,5 @@ from config import Config
 config = Config()
 
 data = fetch_20newsgroups(subset='train')
-df = pd.DataFrame({"text": data.data, "label": data.target})
+df = pd.DataFrame({"text": data.data, "label": data.target}).sample(2000).reset_index(drop=True)
 df.to_csv(config.TRAIN_RAW)
